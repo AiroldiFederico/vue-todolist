42dbc489ad39d79a968ac6acab457ae040ef062e
Federico Airoldi <airoldibusiness@gmail.com>
2023-04-21 22:24:28 +0200

    FASE 0 - importo struttura base

---
+++ asset/js/main.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+const { createApp } = Vue
+
+createApp({
+
+  // VARIABILI VUE
+  data() {
+    return {
+      
+
+
+
+    }
+  },
+
+  //chiamata function al caricamento della pagina
+  created(){
+    
+  },
+
+  // FUNZIONI VUE
+  methods: {
+
+
+
+  }
+
+}).mount('#app')
---
+++ readme.md
@@ -0,0 +1,11 @@
+# ESERCIZIO
+
+### Title: vue-todolist
+---
+**ALUNNO:** FEDERICO AIROLDI
+
+**DATA:** 20 APRILE 2023
+
+**STATO DELL'ESERCIZIO:** WIP
+
+_Boolean Classe #93_
