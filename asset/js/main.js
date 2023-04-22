fdd13bb532f600a22ea1d9338a4d50c2843f94d8
Federico Airoldi <airoldibusiness@gmail.com>
2023-04-22 22:27:20 +0200

    FASE 3 - milestone 3

--- asset/js/main.js
+++ asset/js/main.js
@@ -26,7 +26,7 @@ createApp({
         }
       ],
 
-  
+      TaskText: ''
 
     }
   },
@@ -41,8 +41,14 @@ createApp({
 
     DeleteTask: function(index) {
       this.todos.splice(index, 1);
-    }
+    },
 
+    InserTask: function() {
+      if (this.TaskText) {
+        this.todos.unshift({ text: this.TaskText, done: false });
+        this.newItemText = '';
+      }
+    }
   }
 
 }).mount('#app')
@@ -59,6 +65,8 @@ Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 
  */
